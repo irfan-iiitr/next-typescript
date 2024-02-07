f2eb40bf9901b99b9487643f4f35ffb4ba91e92e
irfan-iiitr <imirfansari@gmail.com>
2024-02-07 15:07:01 +0530

    error 404 not found custom page

--- src/app/product/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/product/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,16 @@
-export default function reviewsOfProduct({params}:{params:{
-    productId:String,
-    reviewId:String
+import { notFound } from "next/navigation"
 
-}}){
+export default function reviewsOfProduct({params}:{params:{
+    productId:string,
+    reviewId:string
+
+}})
+{
+   {
+     if (parseInt(params.reviewId)>1000)
+    notFound();
+}
+
     return (
         <>
         <h1>Review about Product {params.productId} with reviewId {params.reviewId} </h1>
