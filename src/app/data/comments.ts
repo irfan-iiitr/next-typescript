191bcc43f529110e8ed5d7c51f94de896de64897
irfan-iiitr <imirfansari@gmail.com>
2024-02-08 22:50:20 +0530

    Making API's in NextJS

---
+++ src/app/data/comments.ts
@@ -0,0 +1,24 @@
+export const comments= [
+        {
+            "id": 1,
+            "text": "This is the first comment."
+        },
+        {
+            "id": 2,
+            "text": "Here's the second comment."
+        },
+        {
+            "id": 3,
+            "text": "Third comment goes here."
+        },
+        {
+            "id": 4,
+            "text": "This is comment number four."
+        },
+        {
+            "id": 5,
+            "text": "Fifth and final comment."
+        }
+    ]
+
+    // export const commentsJSON = JSON.stringify(comments);
